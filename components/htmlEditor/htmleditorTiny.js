76be1d2a685c27f69d2973ea7b339079358cec63
trixtu <trixtu12@gmail.com>
2023-10-22 00:22:36 +0200

    update

--- components/htmlEditor/htmleditorTiny.js
+++ components/htmlEditor/htmleditorTiny.js
@@ -17,7 +17,7 @@ function HtmleditorTiny({ setText, setValue, value }) {
         init={{
           selector: 'textarea',
           plugins:
-            'mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+            'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
           toolbar:
             'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
           tinycomments_mode: 'embedded',
